a3d2eb33682967ec93950d28e986b43be1693097
yanliang-code <1205836625@qq.com>
2020-11-29 15:21:20 +0800

    11

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      input: ""
+    };
+  },
+  created() {
+    console.log("this====", this);
+  }
+};
+</script>
+
+<style></style>
